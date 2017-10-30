99e0fe21283d562365670c074d8e8b7e9efa97a0
Alexander Kiselev <alexander.kiselev@mail.ru>
2017-10-30 17:02:39 +0300

    Fixes firstscreen & patterns

--- source/js/components/main/steps/firstScreen.js
+++ source/js/components/main/steps/firstScreen.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router';
-import Modal from 'common/modal';
 
 class FirstScreen extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            rulesOpen: false
-        };
-    }
 
     rulesClickHandler = () => this.setState({rulesOpen: true});
 
     rulesModelCloseHandler = () => this.setState({rulesOpen: false});
 
     render() {
-
-        const {rulesOpen} = this.state;
 
         return (
             <div className="firstScreen">
@@ -30,31 +19,19 @@ class FirstScreen extends React.Component {
                             вам ее осуществить
                         </h3>
                         <p>
-                            Создайте ваше персональное,
+                            Создайте ваше персональное
                             видео с помощью конструктора,
                             делитесь им в социальных сетях,
                             собирайте лайки и выигрывайте приз.
                         </p>
                         <Link to="/video/main" className="btn">создать видео</Link>
-                        <a className="rules" href="#!" onClick={this.rulesClickHandler}>Правила конкурса</a>
+                        <a className="rules" href="/rules.pdf" target="_blank">Правила конкурса</a>
                     </div>
+
                     <div className="col">
-                        <div className="videoHolder">
-                            <img src="/static/img/pic011.jpg" alt="" />
-                        </div>
+                        <img src="/static/img/pic009.png" alt="" />
                     </div>
                 </div>
-                <Modal
-                    open={rulesOpen}
-                    title="Правила конкурса"
-                    onClose={this.rulesModelCloseHandler}
-                >
-                    <ul>
-                        <li>1. </li>
-                        <li>2. </li>
-                        <li>3. </li>
-                    </ul>
-                </Modal>
             </div>
         );
     }
